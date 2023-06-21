71d99b8fcc8bfb6d6341cca409b66675542fc90c
Yohann Matus <yohann.matus2101@gmail.com>
2023-06-21 19:22:13 -0400

    cambios para agregar funcion put para actualizar estado de salud

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 //peticion tipo GET
 app.use(express.json());  
 let list = [
-    {id: 1, name: 'TAREA 1'},
-    {id: 2, name: 'TAREA 2'},
+    {id: 1, name: 'Persona 1', estado_salud_actual: 'sano'},
+    {id: 2, name: 'Persona 2',estado_salud_actual: 'lesionado'},
 ]; 
 //Como usuario quiero poder leer las tareas
 app.get('/', (req, res) => {
@@ -19,6 +19,15 @@ app.post('/', (req, res) => {
     const task = req.body; 
     list.push(task);
     res.status(201).send(task);
+}); 
+//Como usuario quiero actualizar una tarea 
+app.put('/:id/estado_salud_actual', (req, res) => { 
+ const {id} = req.params; 
+ const {estado_salud_actual} = req.body; 
+
+ res.json({mensaje: `Se actualizo el estado de salud de la persona con id ${id} a ${estado_salud_actual}`});
+
+
 });
 
 app.listen(PORT, () => {    
